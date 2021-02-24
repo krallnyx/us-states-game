788a384bdc1d845a56d19afc6961e1e0ba9ab6ba
Arnaud Jeanroch <arnaud@jeanroch.fr>
2021-02-24 16:52:34 +0000

    code cleanup using list comprehension

--- main.py
+++ main.py
@@ -12,11 +12,7 @@ guessed_states = []
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)} / 50 States correct", prompt="What's another state name?").title()
     if answer_state == "Exit":
-        missing_states = []
-        for state in data.state.to_list():
-            if state not in guessed_states:
-                missing_states.append(state)
-        pandas.DataFrame(missing_states).to_csv("states_to_learn.csv")
+        pandas.DataFrame([state for state in data.state.to_list() if state not in guessed_states]).to_csv("states_to_learn.csv")
         break
     if answer_state in data.state.to_list():
         if answer_state not in guessed_states:
